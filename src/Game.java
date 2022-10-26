0f66cc9e732b3c7582d1b2e4b4a35ba08abfba27
phlinh2002 <76050015+phlinh2002@users.noreply.github.com>
2022-10-26 19:36:16 +0200

    Add files via upload

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,4 @@
+
   import java.io.BufferedReader;
   import java.io.IOException;
   import java.io.InputStreamReader;
@@ -16,6 +17,7 @@ public class Game {
     int gameLives = 2;
     int P1Losses = 0;
     int P2Losses = 0;
+    public final String end = "---------------";
     public Game (Socket spieler1, Socket spieler2){
         this.spieler1 = spieler1;
         this.spieler2 = spieler2;
@@ -33,16 +35,17 @@ public class Game {
         int leben = 9;
         char[] raten = new char[27];
         int nextGuessIndex = 0;
-        sp1Out.println("Herzlich wilkommen zu Computermaennchen! Du bist Spieler 1!");
+        sp1Out.println("Herzlich wilkommen zu Computermaennchen! Du bist Spieler 1! \n --------------");
         sp1Out.println("Und so geht's: \n Es gitb 2 Spielern. Spieler 1 wählt ein Wort aus, das mindesstens 4 Buchstaben hat, damit Spieler 2 raten kann. Spieler 2 bekommt ein Wort von anderen, welches sie selbst \ndurch das Erraten von Buchstaben versuchen müssen zu entziffern :D \n Aber aufgepasst!! Bei jeder falschen Eingabe, \nwird der unschuldige Roboter mehr zur Verdamnis gebracht! >_< \n Viel Spass!! ");
-        sp2Out.println("Herzlich willkommen zu Computermaennchen! Du bist Spieler 2!");
+        sp2Out.println("Herzlich willkommen zu Computermaennchen! Du bist Spieler 2! \n --------------");
         sp2Out.println("Und so geht's: \n Es gitb 2 Spielern. Spieler 1 wählt ein Wort aus, das mindesstens 4 Buchstaben hat, damit Spieler 2 raten kann. Spieler 2 bekommt ein Wort von anderen, welches sie selbst \ndurch das Erraten von Buchstaben versuchen müssen zu entziffern :D \n Aber aufgepasst!! Bei jeder falschen Eingabe, \nwird der unschuldige Roboter mehr zur Verdamnis gebracht! >_< \n Viel Spass!! ");
-        
+        sp1Out.println(end);
+        sp2Out.println(end);
         while (P1Losses != gameLives && P2Losses != gameLives) {
-            sp1Out.println("Bitte wählen Sie ein Wort aus!");
+            sp1Out.println("Bitte wählen Sie ein Wort aus, das mehr als 6 Buchstaben hat!");
             sp2Out.println("Warten Sie bitte. Spieler 1 wählt ein Wort für Sie aus. In der nächste Runde können Sie ein Wort auswählen.");
 
-            sp1Out.println("SETPHRASE");
+            sp1Out.println("SET");
             sp1Out.flush();
             sp2Out.flush();
             try {
@@ -52,25 +55,25 @@ public class Game {
                 e.printStackTrace();
             }
             sp2Out.println("Ok. Los geht's");
-            sp2Out.println(display(raten));
+            sp2Out.println(check(raten));
             sp2Out.flush();
             while ((!(checkForWin(raten))) && leben != 0) {
                 System.out.println((!(checkForWin(raten))) && leben != 0);
                 System.out.println(leben != 0);
                 System.out.println((!(checkForWin(raten))));
                 System.out.println(leben);
-                sp2Out.println("GUESS");
+                sp2Out.println("ERRATEN");
                 sp2Out.flush();
                 try {
-                    char rate = sp2In.readLine().charAt(0);
-                    System.out.println("Ihr Gegner hat erraetet " + rate);
+                    char rate = sp2In.readLine().toUpperCase().charAt(0);
+                    System.out.println("Ihr Gegner hat erratet " + rate);
                     if (!new String(raten).contains(String.valueOf(rate))){
-                        //if the guess hasn't already been made
+                        
                         raten[nextGuessIndex] = rate;
                         nextGuessIndex++;
                     } else {
                         while ((new String(raten).contains(String.valueOf(rate)))) {
-                            sp2Out.println("GUESSAGAIN");
+                            sp2Out.println("NOCHMALS BITTE");
                             sp2Out.flush();
                             rate = sp2In.readLine().charAt(0);
                         }
@@ -79,9 +82,9 @@ public class Game {
                     }
                     if (geheimesWort.contains(String.valueOf(rate))) {
                         sp2Out.println("Richtig, " + rate + " ist in dem gegebenen Wort");
-                        sp2Out.println(display(raten));
+                        sp2Out.println(check(raten));
                         sp1Out.println("Dein Gegner war richtig " + rate);
-                        sp1Out.println(display(raten));
+                        sp1Out.println(check(raten));
                     } else {
                         sp2Out.println("Nein, " + rate + " ist nicht in dem gegebenen Wort");
                         sp1Out.println("Dein Gegner ist falsch: " + rate);
@@ -112,17 +115,18 @@ public class Game {
                 sp2Out.println("Du hats verloren!");
                 sp1Out.println("Glückwunsch. Du hast gewonnen!");
             } else {
-                switchPlayers(spieler1, spieler2);
+                spielerWechseln(spieler1, spieler2);
                 leben = 6;
                 raten = new char[27];
                 raten[0] = ' ';
             }
         }
-        sp1Out.println("ENDGAME");
-        sp2Out.println("ENDGAME");
+        sp1Out.println("---ENDE---");
+        sp2Out.println("---ENDE---");
     }
 
-    public String display(char[] raten){
+
+    public String check(char[] raten){
         char[] wort = geheimesWort.toCharArray();
         int i = 0;
         for(char c : wort){
@@ -185,7 +189,7 @@ public class Game {
         "\n|"+
         "\n|"+
         "\n|"+
-        "\n˜˜˜");
+        "\n---");
         mannchen[7] = ("\n|"+
         "\n|"+
         "\n|"+
@@ -198,15 +202,15 @@ public class Game {
         "\n---");
         return mannchen[lives];
     }
-    public void switchPlayers(Socket spieler1, Socket spieler2) {
+    public void spielerWechseln(Socket player1, Socket player2) {
         int temp = P1Losses;
         P1Losses = P2Losses;
         P2Losses = temp;
         try {
-            sp1Out = new PrintWriter(spieler2.getOutputStream(), true);
-            sp2Out = new PrintWriter(spieler1.getOutputStream(), true);
-            sp1In = new BufferedReader(new InputStreamReader(spieler2.getInputStream()));
-            sp2In = new BufferedReader(new InputStreamReader(spieler1.getInputStream()));
+            sp1Out = new PrintWriter(player2.getOutputStream(), true);
+            sp2Out = new PrintWriter(player1.getOutputStream(), true);
+            sp1In = new BufferedReader(new InputStreamReader(player2.getInputStream()));
+            sp2In = new BufferedReader(new InputStreamReader(player1.getInputStream()));
         } catch (IOException e){
             e.printStackTrace();
         }
